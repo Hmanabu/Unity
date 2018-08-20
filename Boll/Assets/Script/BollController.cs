ea362e5d34e944823218c402c72cba7fc102b8d7
Hmanabu <fantaorange@outlook.jp>
2018-08-21 02:48:45 +0900

    Add files via upload
    add project

---
+++ Boll/Assets/Script/BollController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BollController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		//以下を追加
+		rb = this.GetComponent<Rigidbody>();
+		rb.AddForce ((transform.forward + transform.right) * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+/*	private void OnCollisionEnter( Collision collision )
+	{
+		rigidbody = GetComponent<Rigidbody>();
+		rigidbody.AddForce ((transform.forward + transform.right) * speed);
+	}*/
+}
